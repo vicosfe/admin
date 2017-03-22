639c5a04de16a312d4f6a0b283184cb1a6d097fc
Victor Osipov <victorosipov99@mail.ru>
2017-03-22 15:39:07 +0300

    add text-redactor

--- js/script.js
+++ js/script.js
@@ -15,5 +15,40 @@ $(document).ready(function(){
 				$(tabAdm).removeClass("active-panel-adm").eq($(this).index()).addClass("active-panel-adm");
 				$(tabItem).hide().eq($(this).index()).fadeIn()
 			}).eq(0).addClass("active-panel-adm"); 
+
+
+			//Превью фото
+			var	prewImgAdmPlace = $(".prewImgAdmPlace");
+			prewImgAdmPlace.change(() => {
+				var preview = document.querySelector('.imagePrev');
+				var files   = document.querySelector('input[type=file]').files;
+
+				function readAndPreview(file) {
+
+    // Расширение у выбранного фото
+    if ( /\.(jpe?g|png|gif)$/i.test(file.name)) {
+    	var reader = new FileReader();
+
+    	reader.addEventListener("load", function () {
+    		var image = new Image();
+    		/*image.height = 190;*/
+    		image.title = file.name;
+    		image.src = this.result;
+    		preview.appendChild( image );
+    	}, false);
+
+    	reader.readAsDataURL(file);
+    };
+
+  };
+
+  if (files) {
+  	[].forEach.call(files, readAndPreview);
+  };
+});
+
 		});
+
+
+
 
